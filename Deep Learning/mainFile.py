87d0a3d4d3ad6c1559d3a4f9bfb1ba038876399b
Felipe Reis <falreis@gmail.com>
2024-05-26 15:43:31 -0300

    Atualização de código de treinamento e criação de arquivo de ambiente

--- Deep Learning/mainFile.py
+++ Deep Learning/mainFile.py
@@ -3,6 +3,14 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 import pprint as ppr
+import datetime
+import os.path
+
+print('Início: ', end='') 
+print(datetime.datetime.now())
+
+my_checkpoint_path = '/home/felipe/Me/doutorado/quicksort_comparison/tmp/checkpt/'
+best_model_name = 'best.pkl'
 
 rng = np.random.RandomState(1234)
 rng_key = jax.random.PRNGKey(rng.randint(2**32))
@@ -29,8 +37,8 @@ def _iterate_sampler(sampler, batch_size):
     while True:
         yield sampler.next(batch_size)
 
-train_sampler = _iterate_sampler(train_sampler, batch_size=32)
-test_sampler = _iterate_sampler(test_sampler, batch_size=100)
+train_sampler = _iterate_sampler(train_sampler, batch_size=16)
+test_sampler = _iterate_sampler(test_sampler, batch_size=32)
 
 proccessor_factory = clrs.get_processor_factory('mpnn', use_ln=True)
 model_params = dict(
@@ -41,8 +49,8 @@ model_params = dict(
     decode_diffs=False,
     hint_teacher_forcing_noise=1.0,
     use_lstm=False,
-    learning_rate=0.001,
-    checkpoint_path='/tmp/checkpt',
+    learning_rate=0.0005,
+    checkpoint_path= my_checkpoint_path,
     freeze_processor=False,
     dropout_prob=0.0,
 )
@@ -56,17 +64,33 @@ model = clrs.models.BaselineModel(
 )
 model.init(dummy_trajectory.features, 1234)
 
+#carrega o último checkpoint
+if os.path.isfile(my_checkpoint_path + best_model_name):
+    model.restore_model(best_model_name)
 
 step = 0
-while step <= 100:
+best_score = 0
+while step <= 500:
     feedback, test_feedback = next(train_sampler), next(test_sampler)
     rng_key, new_rng_key = jax.random.split(rng_key)
     cur_loss = model.feedback(rng_key, feedback)
     rng_key = new_rng_key
-    if step % 10 == 0:
+    if step % 1 == 0:
         predictions_val, _ = model.predict(rng_key, feedback.features)
         out_val = clrs.evaluate(feedback.outputs, predictions_val)
         predictions, _ = model.predict(rng_key, test_feedback.features)
         out = clrs.evaluate(test_feedback.outputs, predictions)
         print(f'step = {step} | loss = {cur_loss} | val_acc = {out_val["score"]} | test_acc = {out["score"]}')
+        print(datetime.datetime.now())
     step += 1
+
+    #armazena os pesos da melhor execução
+    if (out_val["score"] > best_score) or step == 0:
+        best_score = out_val["score"]
+        print('Checkpointing best model')
+        model.save_model(best_model_name)
+
+        #ver https://github.com/google-deepmind/clrs/blob/master/clrs/examples/run.py
+
+print('Fim: ', end='') 
+print(datetime.datetime.now())
