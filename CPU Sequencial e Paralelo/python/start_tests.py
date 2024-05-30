d39336404ce6b8f35797feff28ff881b6402779a
J. Paulo <joaopaulobmata@gmail.com>
2024-05-30 11:40:25 -0300

    testes executados e salvos

--- CPU Sequencial e Paralelo/python/start_tests.py
+++ CPU Sequencial e Paralelo/python/start_tests.py
@@ -1,6 +1,10 @@
 from parallel_vs_serial_sort import main
 
 NUM_OF_TEST = 5
+SAMPLE_ARRAY_SIZES = [100_000,1_000_000,10_000_000]
 
-for x in range(NUM_OF_TEST):
-    main(10_000_000)
+for array_size in SAMPLE_ARRAY_SIZES:
+    print(f'Array Size: {array_size}')
+    for x in range(NUM_OF_TEST):
+        print(f'Test Number #: {x+1}')
+        main(array_size)
